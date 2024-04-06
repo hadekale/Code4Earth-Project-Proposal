b6bdf5b0163c514acf4c4487e3374549c6cb1ea3
Deniz <hakandkale@gmail.com>
2024-04-06 15:13:44 +0300

    Main part done, styling pending

--- PY/main.py
+++ PY/main.py
@@ -1,10 +1,30 @@
 from datetime import datetime
 import plotly.express as px
 from pandas import DataFrame
+from math import sqrt
 
-def makeScatterPlot(asAFunctionOf: str="time", param: str="CO", verificationType: str="bias"):
+settingsDictionary= {"as a function of":("time","lead time"),"parameters":("CO","NO2","SO2","aod500","aod675"),"experiment id": ("oper","mc0074"),"verification type":("bias","mrse"), "time interval":("20201001-20201031","20201101-20201130")}
+
+def checkValues(valueList): 
+    valueCheck={key:"None" for key in valueList}
+    for key in (settingsDictionary.keys()):
+        for value in settingsDictionary[key]:
+            for receivedValue in valueList:
+                if receivedValue==value:
+                    valueCheck[receivedValue]=True
+                
+    for check in valueCheck:
+        if valueCheck[check]!=True:
+            raise Exception(f"The received {check} doesn't match predefined values.")
+        else: print(f"The received {check} matches predefined values.")
+
+
+
+def calculateData(asAFunctionOf: str="time", param: str="CO", verificationType: str="bias", experimentId:str = "oper", timeInterval:str = "20201001-20201031"):
    
-    data = readData(param,asAFunctionOf)
+    checkValues([asAFunctionOf,param,verificationType,experimentId,timeInterval])
+    
+    data = readData(param,asAFunctionOf,experimentId,timeInterval)
     #print(data)
     if verificationType=="bias":
         for key in data.keys():
@@ -12,24 +32,19 @@ def makeScatterPlot(asAFunctionOf: str="time", param: str="CO", verificationType
             for locationIndex in range(len(data[key])):
                 data[key][locationIndex]=data[key][locationIndex][0]-data[key][locationIndex][1]#bias calculation
 
-        for time in data:
-            if len(data[time])<100:
-                print("for time: " + str(time) + " there are " + str(len(data[time])) + " datapoints")   
-                print("\n")
-
 
         for time in data:
             tempFinalBias=0
             for biasValue in data[time]:
-                tempFinalBias+=biasValue/len(data[time])
-            data[time]=tempFinalBias
-        
-        dataForPlot = DataFrame.from_dict(data,orient="index")
-
+                tempFinalBias+=biasValue
+            tempFinalAverageBias=tempFinalBias/len(data[time])
+            data[time]=tempFinalAverageBias
         
         
-        fig = px.line(dataForPlot)
-        fig.show()
+        # for time in data:
+        #     if len(data[time])<100:
+        #         print("for time: " + str(time) + " there are " + str(len(data[time])) + " datapoints")   
+        #         print("\n")
         
         # i=0
         # for time in data:
@@ -40,24 +55,44 @@ def makeScatterPlot(asAFunctionOf: str="time", param: str="CO", verificationType
         #print(DataFrame.from_dict(data))
         # fig = px.scatter(data)
         # fig.show()
+
     elif verificationType=="mrse":
-        return None
+        for key in data.keys():
+            #print(data[key])
+            for locationIndex in range(len(data[key])):
+                data[key][locationIndex]=(data[key][locationIndex][0]-data[key][locationIndex][1])**2#squared bias calculation
+        
+        for time in data:
+            tempSquaredFinalBias=0
+            for biasValue in data[time]:
+                tempSquaredFinalBias+=biasValue
+            tempSquaredFinalAverageBias=tempSquaredFinalBias/len(data[time])
+            data[time]=sqrt(tempSquaredFinalAverageBias)
+
+    return data
+        
+
+def makePlot(data):
+    dataForPlot = DataFrame.from_dict(data,orient="index")  
+    plot = px.line(dataForPlot)
+    return plot
 
 
 
-
-
-def readData(param, asAFunctionOf):
+def readData(param, asAFunctionOf, experimentId,timeInterval):
     if asAFunctionOf=="time":
         timeDict=dict()
-        fileToBeRead=open("A:/Projeler/Code4Earth-Project-Proposal/PY/example_data/20201001-20201031/expid=mc0074,param="+param+".txt","r")
+        wlenString=""
+        if param[:3]=="aod":
+            wlenString = ",wlen=" + param[3:]
+            param="aod"
+        fileToBeRead=openFile(timeInterval,experimentId,param,wlenString)
         for line in fileToBeRead:
             if line[0]=="#": continue
             else:
                 dataList=line.split(",")
                 timeDict[datetime.strptime(dataList[0], '%Y%m%d %H:%M')]=[] #creation of list
-
-        fileToBeRead=open("A:/Projeler/Code4Earth-Project-Proposal/PY/example_data/20201001-20201031/expid=mc0074,param="+param+".txt","r")
+        fileToBeRead=openFile(timeInterval,experimentId,param,wlenString)
         for line in fileToBeRead:
             if line[0]=="#": continue
             else:
@@ -65,16 +100,38 @@ def readData(param, asAFunctionOf):
                 timeDict[datetime.strptime(dataList[0], '%Y%m%d %H:%M')]+=[[float(dataList[5]),float(dataList[6].rstrip("\n"))]] #forecast and observation -- Important! Not everything available for all times in files.
         return(timeDict)
 
-    elif asAFunctionOf=="lead_time":
-        return None
-    else: print("error")
+    elif asAFunctionOf=="lead time":
+        leadTimeDict=dict()
+        wlenString=""
+        if param[:3]=="aod":
+            wlenString = ",wlen=" + param[3:]
+            param="aod"
+        fileToBeRead=openFile(timeInterval,experimentId,param,wlenString)
+        for line in fileToBeRead:
+            if line[0]=="#": continue
+            else:
+                dataList=line.split(",")
+                leadTimeDict[int(float(dataList[1]))]=[] #creation of list
 
+        fileToBeRead=openFile(timeInterval,experimentId,param,wlenString)
+        for line in fileToBeRead:
+            if line[0]=="#": continue
+            else:
+                dataList=line.split(",")
+                leadTimeDict[int(float(dataList[1]))]+=[[float(dataList[5]),float(dataList[6].rstrip("\n"))]] #forecast and observation -- Important! Not everything available for all times in files.
+        return(leadTimeDict)
+    else: print("error")
+    fileToBeRead.close()
+
+
+def openFile(timeInterval, experimentId, param, wlenString):
+    return open(f"A:/Projeler/Code4Earth-Project-Proposal/PY/example_data/{timeInterval}/expid={experimentId},param={param}{wlenString}.txt","r")
 
 
 #timeDict.update(  { datetime.strptime(dataList[0], '%Y%m%d %H:%M'),timeDict[datetime.strptime(dataList[0], '%Y%m%d %H:%M')].append([float(dataList[5]),float(dataList[6].rstrip("\n"))]) })
 
 
-makeScatterPlot()
+makePlot(calculateData()).show()
 
 
 
