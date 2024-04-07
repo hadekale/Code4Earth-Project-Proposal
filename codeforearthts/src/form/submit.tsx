e015e7e5977f32d382a85a3d4127b0b38b9bf52c
yaonok <yavuzarda05@gmail.com>
2024-04-07 14:33:41 +0300

    http request
    XMLHttpRequest setup

--- codeforearthts/src/form/submit.tsx
+++ codeforearthts/src/form/submit.tsx
@@ -7,16 +7,27 @@ import { selectedTimeInterval } from "@/data/timeinterval";
 import { selectedVerificationType } from "@/data/verification";
 
 export default function PlotIt(){
-  `/?asAFunctionOf=${selectedFunction.id}&parameter=${selectedParameter.id}&experimentId=${selectedExperiment.id}&verificationType=${selectedVerificationType.id}&timeInterval=${selectedTimeInterval.id}`
+  
+  
 
-    return <Button onClick={()=>
-      console.log(`/?asAFunctionOf=${selectedFunction.id}&parameter=${selectedParameter.id}&experimentId=${selectedExperiment.id}&verificationType=${selectedVerificationType.id}&timeInterval=${selectedTimeInterval.id}`)}
+  function MyCallback(arg0: string): void {
+    throw new Error("Function not implemented.");
+  }
+
+    return (<Button onClick={()=>{httpGetAsync(`/?asAFunctionOf=${selectedFunction.id}&parameter=${selectedParameter.id}&experimentId=${selectedExperiment.id}&verificationType=${selectedVerificationType.id}&timeInterval=${selectedTimeInterval.id}`,MyCallback)}}
+      
     
-    variant="contained">Plot It</Button>
+    variant="contained">Plot It</Button>)
     
 
+}
 
-
-
+async function httpGetAsync(theUrl: string | URL, callback: (arg0: string) => void) {
+  let xmlHttpReq = new XMLHttpRequest();
+  xmlHttpReq.onreadystatechange = function () {
+    if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) callback(xmlHttpReq.responseText);
+  };
+  xmlHttpReq.open("GET", theUrl, true); // true for asynchronous
+  xmlHttpReq.send(null);
 }
 
